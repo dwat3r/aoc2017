7363dc26c8c8621ea378206708f8d7d08e82a9a8
Oliver Hechtl <hechtloliver@gmail.com>
2018-02-21 00:28:02 +0100

    wip day10

---
+++ day10.hs
@@ -0,0 +1,41 @@
+module Main where
+
+import Control.Monad.ST
+import Data.Array.ST
+import Data.Array.Unboxed
+import qualified Data.Vector as V
+import Debug.Trace
+import Control.Monad
+
+s = V.fromList [14,58,0,116,179,16,1,104,2,254,167,86,255,55,122,244]
+
+newSTUArray :: (Int, Int) -> [Int] -> ST o (STUArray o Int Int)
+newSTUArray = newListArray
+
+part1 = runST $ do
+    list <- newSTUArray (0, 255) [0..255]
+    go 0 0 0 s list
+
+go lp p ss lens list = do 
+    if lp > 15 then do
+        a <- readArray list 0
+        b <- readArray list 1
+        return $ a * b
+    else do
+        debug <- getElems list
+        traceShowM debug
+        let ll = lens V.! lp
+        guard (ll > 1) $ revSubList list p ((p + ll) `mod` 256) (p < (p + ll) `mod` 256)
+        pv <- readArray list p
+        go (lp + 1) ((p + pv + ss) `mod` 256) (ss + 1) lens list
+
+
+revSubList list a b lt = do
+    av <- readArray list a
+    bv <- readArray list b
+    writeArray list a bv
+    writeArray list b av
+    if (lt && (b - a) <= 2) || (not lt && (a - b) <= 2) then return list
+    else revSubList list ((a+1) `mod` 256) (if b == 0 then 255 else b-1) lt
+  
+main = print part1
